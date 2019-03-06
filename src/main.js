2d4aa24a47eaaddbfd701ccea249edccc840cfa6
Evan Moon <bboydart91@gmail.com>
2019-03-06 15:41:19 +0900

    Vuex 설치 및 셋업

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
+
+setTimeout(function () {
+  // Example
+  store.dispatch('exampleAction')
+}, 1000)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store/states.js
@@ -0,0 +1,3 @@
+export default {
+  exampleState: false
+}
