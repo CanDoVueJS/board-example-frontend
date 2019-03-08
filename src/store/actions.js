ac9a00dbcf1289aef39e2ba728773f81729dcaf6
Evan Moon <bboydart91@gmail.com>
2019-03-08 21:40:29 +0900

    api endpoint 변경

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,13 +2,11 @@ import axios from 'axios'
 
 export default {
   signin ({ commit }, payload) {
-    axios.post('http://localhost:8000/signin', {
+    axios.post('http://localhost:8000/api/auth/signin', {
       email: payload.email,
       password: payload.password
     }).then(res => {
-      console.log(res)
-    }).catch(err => {
-      console.log(err)
+      
     })
   }
 }
