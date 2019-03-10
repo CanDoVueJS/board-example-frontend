66f4f0f15c45035acd2f2b7660e2512b8d6e418a
martin <martin@tmon.co.kr>
2019-03-10 16:00:22 +0900

    중복 bootstrap.css 삭제 및 bootstrap-vue.css 삭제

--- src/main.js
+++ src/main.js
@@ -6,9 +6,6 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
