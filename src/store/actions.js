944dbda4edf8486befbe39b2cf813f4fe40632b7
martin <martin@tmon.co.kr>
2019-03-10 16:46:22 +0900

    로그인 성공/실패시 alert 메세지

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,8 +7,11 @@ export default {
       email: payload.email,
       password: payload.password
     }).then(res => {
+      alert('로그인이 완료되었습니다.')
       const { accessToken } = res.data
       commit(SET_ACCESS_TOKEN, accessToken)
+    }).catch(err => {
+      alert(err.response.data.msg)
     })
   }
 }
