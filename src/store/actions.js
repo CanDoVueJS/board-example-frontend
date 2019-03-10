5122c5c4d8221fcfc3e75a083026d0663cd178eb
Evan Windows <bboydart91@gmail.com>
2019-03-10 14:53:38 +0900

    APi -> api로 변경 및 로그인 실패 얼럿 추가

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,14 +1,16 @@
-import API from '@/api'
+import api from '@/api'
 import { SET_ACCESS_TOKEN } from './mutation-types'
 
 export default {
   signin ({ commit }, payload) {
-    API.post('/auth/signin', {
+    api.post('/auth/signin', {
       email: payload.email,
       password: payload.password
     }).then(res => {
       const { accessToken } = res.data
       commit(SET_ACCESS_TOKEN, accessToken)
+    }).catch(err => {
+      alert(err.response.data.msg)
     })
   }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,12 +1,12 @@
 import { SET_ACCESS_TOKEN } from './mutation-types'
-import API from '@/api'
+import api from '@/api'
 
 export default {
   [SET_ACCESS_TOKEN] (state, accessToken) {
     if (accessToken) { // 방어코드
       state.accessToken = accessToken
       // 설명 필요
-      API.defaults.headers.common.Authorization = `jwt ${accessToken}`
+      api.defaults.headers.common.Authorization = `jwt ${accessToken}`
     }
   }
 }
