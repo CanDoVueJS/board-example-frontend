4dc6d25d331436f2ebc4a5c992548c6ef395c49c
martinyounghoonkim <hoons0131@gmail.com>
2019-03-22 07:16:32 +0900

    회원가입시, 비밀번호 검증 폼 추가 및 밸리데이션 추가

--- src/components/SIgnupForm/SIgnupForm.vue
+++ src/components/SIgnupForm/SIgnupForm.vue
@@ -10,6 +10,9 @@
       <input type="password"
              v-model="password"
              placeholder="비밀번호를 입력해주세요."/>
+      <input type="password"
+             v-model="passwordConfirm"
+             placeholder="비밀번호를 다시 입력해주세요." />
       <button type="submit">회원가입</button>
     </fieldset>
   </form>
@@ -27,7 +30,10 @@ export default {
   },
   methods: {
     submit () {
-      const { name, email, password } = this
+      const { name, email, password, passwordConfirm } = this
+      if (password !== passwordConfirm) {
+        return false
+      }
       this.$emit('submit', { name, email, password })
     }
   }
