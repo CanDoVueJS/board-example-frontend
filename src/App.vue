7c28502da52e80f8f075c5e2428de4d21c42ecf1
Evan Moon <bboydart91@gmail.com>
2019-03-24 16:05:36 +0900

    컴포넌트 스타일 롤백

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,37 @@ export default {
 </script>
 
 <style>
-  @import 'http://localhost:8000/assets/index.css';
+  @import "https://use.fontawesome.com/releases/v5.6.3/css/all.css";
+  // 추후 스타일 작업이 완료되면 웹팩 빌드 후 다시 살려야함 - 2019.03.24 Evan
+  // 반드시 맨 위로 올릴 것. css의 @import 구문은 맨 위에 위치해야만 작동한다.
+  /*@import 'http://localhost:8000/assets/index.css'*/
+
+  html, body {
+    height: 100%;
+    background-color: #f9f9f9;
+  }
+  html, body, div, input, fieldset, form, h1, p, textarea, button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+  textarea {
+    border: none;
+    resize: none;
+  }
+  ul, li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    height: 100%;
+  }
 </style>
