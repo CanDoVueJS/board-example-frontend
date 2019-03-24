04adbd196603c8d716afde521ca5327300a30a57
Evan Moon <bboydart91@gmail.com>
2019-03-24 23:23:16 +0900

    토큰을 SET할 때 쿠키에도 저장하도록 변경

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,11 @@
-import {DESTROY_ACCESS_TOKEN, DESTROY_MY_INFO, SET_ACCESS_TOKEN, SET_MY_INFO} from './mutation-types'
+import {
+  SET_ACCESS_TOKEN,
+  SET_MY_INFO,
+  DESTROY_ACCESS_TOKEN,
+  DESTROY_MY_INFO
+} from './mutation-types'
 import api from '@/api'
+import Cookies from 'js-cookie'
 
 export default {
   [SET_ACCESS_TOKEN] (state, accessToken) {
@@ -7,6 +13,7 @@ export default {
       state.accessToken = accessToken
       // 설명 필요
       api.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+      Cookies.set('accessToken', accessToken, { expires: 1 })
     }
   },
   [SET_MY_INFO] (state, me) {
@@ -17,6 +24,7 @@ export default {
   [DESTROY_ACCESS_TOKEN] (state) {
     state.accessToken = ''
     delete api.defaults.headers.common.Authorization
+    Cookies.remove('accessToken')
   },
   [DESTROY_MY_INFO] (state) {
     state.me = null
