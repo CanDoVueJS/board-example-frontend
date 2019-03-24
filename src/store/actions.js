b24494d07ec364f0a174e4d3475cb863d821a179
Evan Moon <bboydart91@gmail.com>
2019-03-24 23:27:38 +0900

    앱 초기화 시 저장된 토큰을 사용하여 로그인할 수 있도록 수정

--- src/store/actions.js
+++ src/store/actions.js
@@ -25,5 +25,15 @@ export default {
   signout ({ commit }) {
     commit(DESTROY_MY_INFO)
     commit(DESTROY_ACCESS_TOKEN)
+  },
+  signinByToken ({ commit }, token) {
+    commit(SET_ACCESS_TOKEN, token)
+    return api.get('/users/me')
+      .then(res => {
+        commit(SET_MY_INFO, res.data)
+        return Promise.resolve(res.data)
+      }).catch(err => {
+        return Promise.reject(err)
+      })
   }
 }
