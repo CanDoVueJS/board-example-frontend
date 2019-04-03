09cb74f7663b8aad9d388984a7172d07b6b6c9bf
martinyounghoonkim <hoons0131@gmail.com>
2019-04-04 00:17:02 +0900

    게시판 댓글 리스트가 없을 경우에 대한 조건 추가

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,8 +1,11 @@
 <template>
-  <ul v-if="comments.length > 0">
+  <ul>
     <li v-for="comment in comments" :key="comment.id">
       <strong>{{ comment.user.name }}</strong><span>{{ comment.createdAt }}</span>
       <p>{{ comment.contents }}</p>
+    </li>
+    <li v-show="comments.length <= 0">
+      입력된 댓글이 없습니다.
     </li>
   </ul>
 </template>
@@ -11,7 +14,10 @@ export default {
   name: 'CommentList',
   props: {
     comments: {
-      type: Array
+      type: Array,
+      default () {
+        return [];
+      }
     }
   }
 }
